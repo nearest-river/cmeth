7819b19ffe70e8318dc727c20ad415b4feb308ee
Nate River <trishiragolder69@outlook.com>
2024-10-31 06:51:30 +0530

    added defination of Vec3

---
+++ src/vec3.h
@@ -0,0 +1,37 @@
+#include "prelude.h"
+
+
+typedef struct {
+  f32 x;
+  f32 y;
+  f32 z;
+} Vec3;
+
+
+const Vec3 vec3(f32 x,f32 y,f32 z);
+const Vec3 vec3_new(f32 x,f32 y,f32 z);
+const Vec3 vec3_splat(f32 v);
+const Vec3 vec3_map(Vec3 self,f32 (*f)(f32));
+const Vec3 vec3_from_array(f64 a[3]);
+const void vec3_write_to_slice(Vec3 self,f32* slice);
+const Vec3 vec3_from_vec4(f32 v[4]);
+const f32 vec3_dot(Vec3 self,Vec3 rhs);
+const Vec3 vec3_dot_into_vec(Vec3 self,Vec3 rhs);
+const Vec3 vec3_cross(Vec3 self,Vec3 rhs);
+const Vec3 vec3_min(Vec3 self,Vec3 rhs);
+const Vec3 vec3_max(Vec3 self,Vec3 rhs);
+const f32 vec3_min_element(Vec3 self);
+const f32 vec3_max_element(Vec3 self);
+const f32 vec3_element_sum(Vec3 self);
+const f32 vec3_element_product(Vec3 self);
+const Vec3 vec3_abs(Vec3 self);
+const Vec3 vec3_signum(Vec3 self);
+const Vec3 vec3_copysign(Vec3 self,Vec3 rhs);
+const u32 vec3_is_negative_bitmask(Vec3 self);
+const bool vec3_is_finite(Vec3 self);
+const bool vec3_is_nan(Vec3 self);
+const f32 vec3_len(Vec3 self);
+const f32 vec3_len_squared(Vec3 self);
+
+
+
