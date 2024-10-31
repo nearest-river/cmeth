8da9f9778ed1408a63c246e03ddd8ddf0d524e1c
Nate River <trishiragolder69@outlook.com>
2024-10-31 06:48:42 +0530

    init

---
+++ src/prelude.h
@@ -0,0 +1,12 @@
+#include "../include/cprimitives/src/lib.h"
+
+
+#define inline_always __inline __attribute__ ((__always_inline__))
+#define min(X,Y) X<Y?X:Y
+#define max(X,Y) X>Y?X:Y
+
+
+
+
+
+
---
+++ tests/main.c
@@ -0,0 +1,6 @@
+#include "../src/vec3.h"
+
+int main() {
+  return 0;
+}
+
