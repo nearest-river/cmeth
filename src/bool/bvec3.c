4461ed16ba6824db6c2f14248ad4fe8dfe9c16b3
Nate River <trishiragolder69@outlook.com>
2024-11-05 21:43:55 +0530

    added BVec3 and basic methods

---
+++ src/bool/bvec3.c
@@ -0,0 +1,94 @@
+#include "bvec3.h"
+
+/// Creates a 3-dimensional `bool` vector mask.
+inline_always
+const BVec3 bvec3(bool x, bool y, bool z) {
+  return bvec3_new(x,y,z);
+}
+
+/// Creates a new vector mask.
+inline_always
+const BVec3 bvec3_new(bool x, bool y, bool z) {
+  BVec3 vec={
+    .x=x,
+    .y=y,
+    .z=z
+  };
+  return vec;
+}
+
+/// Creates a vector mask with all elements set to `v`.
+inline
+const BVec3 bvec3_from_array(bool a[3]) {
+  return bvec3_new(a[0],a[1],a[2]);
+}
+
+/// Returns a bitmask with the lowest 3 bits set from the elements of `self`.
+///
+/// A true element results in a `1` bit and a false element in a `0` bit.  Element `x` goes
+/// into the first lowest bit, element `y` into the second, etc.
+inline
+const u32 bvec3_bitmask(const BVec3 self) {
+  return ((u32)(self.x)) | ((u32)(self.y)) << 1 | ((u32)(self.z)) << 2;
+}
+
+/// Returns true if any of the elements are true, false otherwise.
+inline
+const bool bvec3_any(const BVec3 self) {
+  return self.x || self.y || self.z;
+}
+
+/// Returns true if all the elements are true, false otherwise.
+inline
+const bool bvec3_all(const BVec3 self) {
+  return self.x && self.y && self.z;
+}
+
+/// Tests the value at `index`.
+/// 
+/// Panics if `index` is greater than 2.
+inline
+const bool bvec3_test(const BVec3 self,usize index) {
+  switch(index) {
+    case 0: return self.x;
+    case 1: return self.y;
+    case 2: return self.z;
+    default: panic("index out of bounds")
+  }
+}
+
+/// Sets the element at `index`.
+///
+/// Panics if `index` is greater than 2.
+inline
+void bvec3_set(BVec3* self,usize index,bool value) {
+  switch(index) {
+    case 0:
+      self->x=value;
+    break;
+    case 1:
+      self->y=value;
+    break;
+    case 2:
+      self->z=value;
+    break;
+    default: panic("index out of bounds")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/bool/bvec3.h
@@ -0,0 +1,55 @@
+#ifndef CMETH_BVEC3_PRELUDE_H
+#define CMETH_BVEC3_PRELUDE_H
+#include "../prelude.h"
+
+/// A 3-dimensional `bool` vector mask.
+typedef struct {
+  bool x;
+  bool y;
+  bool z;
+} BVec3;
+
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+/// Creates a 3-dimensional `bool` vector mask.
+const BVec3 bvec3(bool x, bool y, bool z);
+
+/// Creates a new vector mask.
+const BVec3 bvec3_new(bool x, bool y, bool z);
+
+/// Creates a vector mask with all elements set to `v`.
+const BVec3 bvec3_from_array(bool a[3]);
+
+/// Returns a bitmask with the lowest 3 bits set from the elements of `self`.
+///
+/// A true element results in a `1` bit and a false element in a `0` bit.  Element `x` goes
+/// into the first lowest bit, element `y` into the second, etc.
+const u32 bvec3_bitmask(const BVec3 self);
+
+/// Returns true if any of the elements are true, false otherwise.
+const bool bvec3_any(const BVec3 self);
+
+/// Returns true if all the elements are true, false otherwise.
+const bool bvec3_all(const BVec3 self);
+
+/// Tests the value at `index`.
+/// 
+/// Panics if `index` is greater than 2.
+const bool bvec3_test(const BVec3 self,usize index);
+
+/// Sets the element at `index`.
+///
+/// Panics if `index` is greater than 2.
+void bvec3_set(BVec3* self,usize index,bool value);
+
+
+
+
+#ifdef _cplusplus
+}
+#endif
+
+
+#endif
